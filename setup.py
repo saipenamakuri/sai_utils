7620b3082911a83695eac26c930b2a64f66c94a2
sai.penamakuri <sai.penamakuri@sparshik.com>
2020-08-05 21:11:53 +0530

    corrected download url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Sai',                   # Type in your name
   author_email = 'SaiPenamakuri@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/saipenamakuri/sai_utils',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/saipenamakuri/sai_utils/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['utilities'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'PIL'
