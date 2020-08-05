8fa8cad9f3c4713a3a3e20c6b812a3ba82deb8f0
sai.penamakuri <sai.penamakuri@sparshik.com>
2020-08-05 21:19:45 +0530

    added required files for upload to PyPi

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from distutils.core import setup
 setup(
   name = 'sai_utils',         # How you named your package folder (MyLib)
   packages = ['sai_utils'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Practice library for publishing to PyPi',   # Give a short description about your library
   author = 'Sai',                   # Type in your name
   author_email = 'SaiPenamakuri@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/saipenamakuri/sai_utils',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/saipenamakuri/sai_utils/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/saipenamakuri/sai_utils/archive/v_02.tar.gz',    # I explain this later on
   keywords = ['utilities'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'PIL'
+          'Pillow'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
